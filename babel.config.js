8e384b7d84e9a1425fa6aea1c1e12437bf04a7b0
Ravid Rom <romravid@gmail.com>
2022-06-06 19:37:24 +0300

    Mobx, babel reload issue work around

--- babel.config.js
+++ babel.config.js
@@ -1,8 +1,23 @@
 module.exports = {
 	presets: ['module:metro-react-native-babel-preset'],
 	/* This line was required by the Mobx installation steps:
+		plugins: [['@babel/plugin-proposal-class-properties', {loose: false}]],
 		https://mobx.js.org/installation.html
-		but made a bug were prints and network requests wont work
+
+		but made a bug were prints and network requests wont work...
+
+		The work around:
+		https://github.com/mobxjs/mobx/issues/3100#issuecomment-962605244
 	 */
-	// plugins: [['@babel/plugin-proposal-class-properties', {loose: false}]],
+	overrides: [
+		{
+			test: fileName => !fileName.includes('node_modules'),
+			plugins: [
+				[
+					require('@babel/plugin-proposal-class-properties'),
+					{loose: false},
+				],
+			],
+		},
+	],
 };
