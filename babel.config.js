966e823431cb3a9e36d4a9d7d2cd739234ffd397
Ravid Rom <romravid@gmail.com>
2022-06-06 19:18:28 +0300

    Fixed idle network requests bug

--- babel.config.js
+++ babel.config.js
@@ -1,4 +1,8 @@
 module.exports = {
 	presets: ['module:metro-react-native-babel-preset'],
-	plugins: [['@babel/plugin-proposal-class-properties', {loose: false}]],
+	/* This line was required by the Mobx installation steps:
+		https://mobx.js.org/installation.html
+		but made a bug were prints and network requests wont work
+	 */
+	// plugins: [['@babel/plugin-proposal-class-properties', {loose: false}]],
 };
