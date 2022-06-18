7536a2a201c53be6ab9950bf3f093530bb42d3aa
ravidro <ravidro@post.bgu.ac.il>
2022-06-19 01:39:09 +0300

    Logging out on expired token

--- src/components/Views/ConnectView.tsx
+++ src/components/Views/ConnectView.tsx
@@ -17,7 +17,7 @@ type LoginViewProps = {
 };
 
 export default function LoginView(props: LoginViewProps) {
-	if (props.isConnected) {
+	if (props.isConnected && props.loggedIn) {
 		return (
 			<>
 				{props.isReconnecting && (
